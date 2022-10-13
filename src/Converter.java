dd728d179801fd78905cf8d85ec519577fbc13ac
alexx-d <95764790+alexx-d@users.noreply.github.com>
2022-10-13 11:20:04 +0300

    Method main is written + minor improvements

--- src/Converter.java
+++ src/Converter.java
@@ -1,36 +1,35 @@
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Converter {
-    HashMap<String, Integer> types;
+    HashMap<String, Integer> numSystem;
     Double[][] multipliers = {
-            {1.0, 0.000621371, 3.28084, 39.3701, 0.7112},
+            {1.0, 0.000621371, 3.28084, 39.3701, 1.40607424071991},
             {1609.34, 1.0, 5280.0, 63360.0, 1144.562608},
             {0.3048, 0.000189394, 1.0, 12.0, 0.21677376},
             {0.0254, 1.5783e-5, 0.0833333, 1.0, 0.01806448},
-            {0.7112, 0.0004419190552, 2.333333408, 28.00001512, 1.0}
+            {1.40607424071991, 0.0004419190552, 2.333333408, 28.00001512, 1.0}
     };
     public Converter() {
-        types = new HashMap<>();
-        types.put("meters", 0);
-        types.put("miles", 1);
-        types.put("foots", 2);
-        types.put("inches", 3);
-        types.put("arshin", 4);
+        numSystem = new HashMap<>();
+        numSystem.put("meters", 0);
+        numSystem.put("miles", 1);
+        numSystem.put("foots", 2);
+        numSystem.put("inches", 3);
+        numSystem.put("arshins", 4);
     }
 
-
     double convert(double value, String from, String to){
-        return value * (multipliers[types.get(from)][types.get(to)]);
+        return value * (multipliers[numSystem.get(from)][numSystem.get(to)]);
     }
 
     String getType(){
         final Scanner scanner = new Scanner(System.in);
         String type;
         do{
-            System.out.println("Enter numbering system: ");
-            type = scanner.nextLine();
-        } while(!types.containsKey(type));
+            type = scanner.nextLine().toLowerCase(Locale.ROOT);
+        } while(!numSystem.containsKey(type));
         return type;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        final Scanner scanner = new Scanner(System.in);
+        Converter converter = new Converter();
+        System.out.print("Enter the number you want to convert: ");
+        double value = scanner.nextDouble();
+        System.out.print("Enter the number system of your number: ");
+        String originalNumSystem = converter.getType();
+        System.out.print("Enter the number system to which you want to convert: ");
+        String newNumSystem = converter.getType();
+        System.out.println(value + " " + originalNumSystem + " = "
+                + converter.convert(value,originalNumSystem,newNumSystem) + " " + newNumSystem);
     }
 }
